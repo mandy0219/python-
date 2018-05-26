3af4844fc9e2423338573d51ea84274f587dc716
Wu Hsiu Jung <mandy19970219@gm.student.ncut.edu.tw>
2018-05-26 15:45:39 +0800

    python繪圖

---
+++ history.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# *** Spyder Python Console History Log ***
+
+## ---(Sat May 26 14:58:04 2018)---
+runfile('C:/Users/Student/.spyder-py3/temp.py', wdir='C:/Users/Student/.spyder-py3')
---
+++ temp.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import matplotlib.pyplot as plt
+plt.plot([1,2,3,4],'r')
+plt.show()
